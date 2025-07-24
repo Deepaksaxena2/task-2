afde193d7dbe21ccc7a872fb46b4c3d2425192de
Deepaksaxena2 <deepaksaxena0367@gmail.com>
2025-07-23 22:56:13 -0700

    Add files via upload

---
+++ deepak.py3.py
@@ -0,0 +1,29 @@
+#perform basic mathematical operations:
+p=input('enter a value:')
+n=input('enter a value:')
+p=int(p)
+n=int(n)
+sum_result=p+n
+print(sum_result)
+#
+p=input('enter a value:')
+n=input('enter a value:')
+p=int(p)
+n=int(n)
+different_result=p-n
+print(different_result)
+#
+p=input('enter a value:')
+n=input('enter a value:')
+p=int(p)
+n=int(n)
+product_result=p*n
+print(product_result)
+#
+p=input('enter a value:')
+n=input('enter a value:')
+p=int(p)
+n=int(n)
+division_result=p/n
+print(division_result)
+#
